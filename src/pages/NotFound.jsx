6421b7dd4f399926d28cc8b41b6ff72342bae6d4
Sankar K G <sankarfrompalani@gmail.com>
2025-07-27 23:36:05 +0530

    style: update NotFound component with 404 message and description

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const NotFound = () => {
   return (
     // This is a placeholder for the NotFound component
-    <div>NotFound</div>
+    <div>
+      <h1>404</h1>
+      <h1>Not Found </h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
   )
 }
 
